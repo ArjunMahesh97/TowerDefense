383246d763850251c1df702d4ca10194de03dfc6
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-05-15 18:23:22 +0530

    snap

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorSnap : MonoBehaviour {
+
+    [ExecuteInEditMode]
+
+    private void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * 10f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * 10f;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
