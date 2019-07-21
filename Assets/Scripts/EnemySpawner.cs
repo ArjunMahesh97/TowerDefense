0523396d6076e125dbd54e15ee769583fbb076c4
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-07-21 23:18:44 +0530

    spawning

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] float secondsBetweenSpawns = 5f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemies());
+	}
+	
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
